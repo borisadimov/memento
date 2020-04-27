e1de4a56ee0091cdd92869b6055c9e584e0e99ab
Boris Adimov <boris@adimov.ru>
2020-04-27 22:40:33 +0300

    Updates

---
+++ precache-manifest.e2eb378393578096d46c03e59078f45f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4520ea1816fa7cde92f3d0baf0bbac3",
+    "url": "/memento/index.html"
+  },
+  {
+    "revision": "5e943712624f01ce0f94",
+    "url": "/memento/static/css/main.0fa55258.chunk.css"
+  },
+  {
+    "revision": "f574466126fabfc2ea88",
+    "url": "/memento/static/js/2.ffe6506c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memento/static/js/2.ffe6506c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e943712624f01ce0f94",
+    "url": "/memento/static/js/main.c97efd58.chunk.js"
+  },
+  {
+    "revision": "e59b94b164feb35d1017",
+    "url": "/memento/static/js/runtime-main.5da6bd48.js"
+  }
+]);
